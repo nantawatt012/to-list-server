ea2385bb73395f8e2ffea89caafe1e90783215b8
nantawatt012 <nantawatt012@gmail.com>
2022-12-23 10:19:40 +0700

    set all component

--- src/App.js
+++ src/App.js
@@ -9,34 +9,6 @@ function App() {
       <ul className="list-group">
         <TotoItem />
       </ul>
-
-      {/* <li className="list-group-item p-3">
-          <form>
-            <div className="input-group">
-              <input type="text" className="form-control" value="Play game" />
-              <button className="btn btn-primary">
-                <i className="fa-solid fa-check" />
-              </button>
-              <button className="btn btn-secondary">
-                <i className="fa-solid fa-xmark" />
-              </button>
-            </div>
-          </form>
-        </li>
-        <li className="list-group-item p-3 text-bg-success">
-          <div className="d-flex align-items-center">
-            <span className="flex-fill">Meet the dentist</span>
-            <div className="btn-group">
-              <button className="btn btn-outline-light">
-                <i className="fa-solid fa-repeat" />
-              </button>
-              <button className="btn btn-outline-light">
-                <i className="fa-regular fa-trash-can" />
-              </button>
-            </div>
-          </div>
-        </li>
-      </ul> */}
     </div>
   );
 }
--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -1,7 +1,12 @@
-// import Task from "./task";
+import Task from "./Task";
+import TodoForm from "./TodoForm";
+// import TodoForm from "./TodoForm";
 
 export default function TotoItem() {
   return (
-    <li className="list-group-item p-3 text-bg-danger">{/* <Task /> */}</li>
+    <li className="list-group-item p-3 text-bg-danger">
+      {/* <TodoForm /> */}
+      <Task />
+    </li>
   );
 }
